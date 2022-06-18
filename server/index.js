bb08502ef0a98953acdd9bec6bce577610668f67
chana1024 <linzhinan1024@gmail.com>
2022-06-18 15:47:52 +0800

    vue code link 11

--- server/index.js
+++ server/index.js
@@ -2,16 +2,19 @@ const child_process = require('child_process')
 const pathUtil = require("../utils/pathUtil.js")
 
 function openCodeFile(path) {
-  let filePath = pathUtil.projectBasePath + path
-  child_process.exec(`code -r -g ${filePath}`)
+    let filePath = pathUtil.projectBasePath + path
+    child_process.exec(`code -r -g ${filePath}`)
 }
 module.exports = {
-  before: function(app) {
-    app.get('/code', function(req, res) {
-      if (req.query.filePath) {
-        // 执行vscode定位代码行命令
-        openCodeFile(req.query.filePath)
-      }
-    })
-  }
+    before: function(app) {
+        if (process.env.NODE_ENV === 'development') {
+            app.get('/code', function(req, res) {
+                if (req.query.filePath) {
+                    // 执行vscode定位代码行命令
+                    openCodeFile(req.query.filePath)
+                }
+                res.send('successfully receive request')
+            })
+        }
+    }
 }
