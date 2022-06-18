967bb9445b9e36449c796a5c06c2d5be0f71f90d
chana1024 <linzhinan1024@gmail.com>
2022-06-18 14:57:42 +0800

    click ball

--- client/index.js
+++ client/index.js
@@ -1,4 +1,34 @@
 const axios = require('axios')
+const indexcss = require("../css/index.css")
+
+function uuid(len, radix) {
+  var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+  var uuid = [],
+    i;
+  radix = radix || chars.length;
+  if (len) {
+    // Compact form
+    for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
+  } else {
+    // rfc4122, version 4 form
+    var r;
+
+    // rfc4122 requires these characters
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+    uuid[14] = '4';
+
+    // Fill in random data.  At i==19 set the high bits of clock sequence as
+    // per rfc4122, sec. 4.1.5
+    for (i = 0; i < 36; i++) {
+      if (!uuid[i]) {
+        r = 0 | Math.random() * 16;
+        uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+      }
+    }
+  }
+
+  return uuid.join('');
+}
 
 function sendRequestToOpenFileInEditor(filePath) {
   const port = process.env.port || process.env.npm_config_port || 80 // 端口
@@ -36,6 +66,17 @@ function init() {
     }
     document.onmousedown = function(e) {
       if (e.shiftKey && e.button === 2) {
+        const element = document.createElement('div');
+        const elementId = "clickBall" + uuid(8, 16);
+        element.id = elementId;
+        element.style.left = (e.clientX - 20) + "px";
+        element.style.top = (e.clientY - 20) + "px";
+        element.setAttribute("class", "clickBall hidSlow");
+
+        document.body.appendChild(element);
+        setTimeout(() => {
+          document.getElementById(elementId).remove();
+        }, 1000)
         openFileInEditor(e)
       }
     }
